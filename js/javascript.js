bd4f55dde98e9a92ba627d9ed3df81475b7a5907
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-22 18:13:01 +0100

    Hogwarts Express

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,21 @@
 const userKm = document.getElementById('kilometres');
 const userAge = document.getElementById('age');
-const input = document.getElementById('button');
+const input = document.querySelector('a.btn');
+const output = document.getElementById('price-output');
+
 
 input.addEventListener('click', 
     function(){
-        console.log(userKm.value);
-        console.log(userAge.value);
         let ticketPrice = userKm.value * 0.2762;
-        console.log(ticketPrice.toFixed(2));
         if (userAge.value < 18){
             ticketPrice = ticketPrice * 0.825;
-            console.log(ticketPrice.toFixed(2));
+            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
+            
         }else if (userAge.value >= 65){
             ticketPrice = ticketPrice * 0.667;
-            console.log(ticketPrice.toFixed(2))
+            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
+        }else {
+            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
         }
 })
+
