2e5fe2075b8bea85b5868770cdc8c09f2dfd1c1d
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-23 14:34:28 +0100

    edit js

--- js/javascript.js
+++ js/javascript.js
@@ -8,14 +8,10 @@ input.addEventListener('click',
     function(){
         let ticketPrice = userKm.value * 0.2762;
         if (userAge.value < 18){
-            ticketPrice = ticketPrice * 0.825;
-            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
-            
+            ticketPrice = ticketPrice * 0.825; //17.5%
         }else if (userAge.value >= 65){
-            ticketPrice = ticketPrice * 0.667;
-            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
-        }else {
-            output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
-        }
+            ticketPrice = ticketPrice * 0.667; //33.3%
+        } 
+        output.innerHTML = ticketPrice.toFixed(2) + "&euro;";
 })
 
